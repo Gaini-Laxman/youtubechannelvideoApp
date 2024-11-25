139ee3b5500c311c3d4ccd0d3d5159ed68e05e80
Gaini Laxman <gainilaxman12@gmail.com>
2024-11-25 23:40:15 +0530

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,39 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'youtubechannelapi';
+  videos = [
+    { 
+      videoId: 'Su2eUTLR984', 
+      title: 'Java Introduction'
+    },
+    { 
+      videoId: 'WlVCG4wqhpk', 
+      title: 'Java Programs Development'
+    },
+    {
+      videoId: 'QswFHYgdFk8',
+      title: 'Variables, Data Types, Identifiers & Rules'
+    },
+    {
+      videoId: 'rYPEfz1c0ec',
+      title: 'Reserved keyWords, Java Coding Standards, Java comments, Reading Data From keyboards'
+    },
+    {
+      videoId: 'vC0xywgc9Yk',
+      title: 'Operators, Arithmetic, Relational, Logical Assignment'
+    },
+    {
+      videoId: 'k9Gmkt0QxIQ',
+      title: 'Operators, Arithmetic, Relational, Logical Assignment'
+    },
+      {
+        videoId: 'qCtcCAa8TmM',
+        title: 'Operators, Arithmetic, Relational, Logical Assignment'
+      },
+      {
+        videoId: 'F2aOWwSCnYc',
+        title: ' Arrays, String, StringBuffer, StringBuffer'
+      }
+    
+  ];
 }
